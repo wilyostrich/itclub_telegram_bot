9f75eb29ad9cdbbc35c5774a84370abafdf7241c
vladislav <vladislav.lants@gmail.com>
2016-09-06 16:35:10 +0400

    up

--- bot.py
+++ bot.py
@@ -23,7 +23,7 @@ def getMessage():
 @server.route("/")
 def webhook():
     bot.remove_webhook()
-    bot.set_webhook(url="https://herokuProject_url/bot")
+    bot.set_webhook(url="https://test-bo.herokuapp.com/bot")
     return "!", 200
 
 server.run(host="0.0.0.0", port=os.environ.get('PORT', 5000))
